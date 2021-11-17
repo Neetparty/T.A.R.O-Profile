c9fd6316386a47f5f7fa842f98c43f6550a4aa95
Hitorin <phantomstudio299@gmail.com>
2021-11-17 16:50:35 +0700

    Header ( Incomplete ) @ Hitorin

--- comps/Header.js
+++ comps/Header.js
@@ -1,8 +1,32 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
 const header = () => {
+    const [value, setValue] = React.useState('neet');
+    const [color, setColor] = React.useState('secondary');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+        setColor(event.target.attributes.color.value)
+    };
     return ( 
-        <nav>
-            <h1>Hi I'm Nav</h1>
-        </nav>
+        <Box sx={{ width: '100%', height: '0', m: '1em', p: '0' }}>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                textColor={color}
+                indicatorColor="secondary"
+                sx={{ p: '1.5em', pt: '0', pb: '0' }}
+            >
+                <Tab sx={{ width: '20%', my: 0.5 }} value="neet" color="secondary" label="Neetparty" />
+                <Tab sx={{ width: '20%', my: 0.5 }} value="zcross" color="primary" label="Hoshina" />
+                <Tab sx={{ width: '20%', my: 0.5 }} value="neverterra" color="success" label="Neverterra" />
+                <Tab sx={{ width: '20%', my: 0.5 }} value="taezo" color="info" label="TaeZo_" />
+                <Tab sx={{ width: '20%', my: 0.5 }} value="moonsky" color="danger" label="MoonSky" />
+            </Tabs>
+        </Box>
      );
 }
  
