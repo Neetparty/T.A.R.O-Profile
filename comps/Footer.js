b4ba2344fca7e67b2d9281bc242f354ec3c33dea
HOSHINANB\nlove <nlove0025@gmail.com>
2021-11-19 09:28:54 +0700

    Footer complate

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,9 +1,17 @@
+import { palette } from '@mui/system';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 const footer = () => {
-    return ( 
+    return (
         <div>
-            <a>Hi I'm Footer</a>
+          <Typography component="div">
+            <Box sx={{ bgcolor: 'primary.main', color: 'primary.contrastText', p: 2 }}>
+              <Box sx={{ textAlign: 'right', m: 1 }}>Copyright @ H0shiNa</Box>
+            </Box>
+          </Typography>
         </div>
      );
 }
- 
+
 export default footer;
